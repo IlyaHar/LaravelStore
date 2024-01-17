5fdd0789825b50129f814ec9ff3d885920818575
IlyaHar <harcenkoila278@gmail.com>
2024-01-17 09:04:25 +0200

    add users

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\Roles;
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        if (!User::where('email', env('ADMIN_EMAIL'))->exists()) {
+            User::factory()->withEmail(env('ADMIN_EMAIL'))->create()->syncRoles(Roles::ADMIN);
+        }
+    }
+}
